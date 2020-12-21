9c4c428f39cd3aa4d1e163a77c6c3450ce3e0661
yc-How-python <66516657+yc-How-python@users.noreply.github.com>
2020-12-21 16:27:36 +0800

    Create vmdscript.tcl

---
+++ vmdscript.tcl
@@ -0,0 +1,126 @@
+mol new J:\\TrjData\\workstation\\64me_290\\0_0\\0_0.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_0\\0_0.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_0.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_1\\0_1.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_1\\0_1.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_1.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_2\\0_2.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_2\\0_2.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_2.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_2\\0_2.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_3\\0_3.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_2.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_4\\0_4.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_4\\0_4.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_4.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_5\\0_5.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_5\\0_5.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_5.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_6\\0_6.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_6\\0_6.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_6.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_7\\0_7.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_7\\0_7.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_7.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_8\\0_8.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_8\\0_8.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_8.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\0_9\\0_9.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\0_9\\0_9.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\0_9.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_0\\1_0.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_0\\1_0.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_0.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_1\\1_1.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_1\\1_1.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_1.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_2\\1_2.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_2\\1_2.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_2.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_3\\1_3.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_3\\1_3.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_3.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_4\\1_4.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_4\\1_4.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_4.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_5\\1_5.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_5\\1_5.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_5.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_6\\1_6.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_6\\1_6.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_6.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_7\\1_7.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_7\\1_7.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_7.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_8\\1_8.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_8\\1_8.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_8.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\1_9\\1_9.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\1_9\\1_9.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\1_9.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
+mol new J:\\TrjData\\workstation\\64me_290\\2_0\\2_0.gro
+animate delete all
+mol addfile J:\\TrjData\\workstation\\64me_290\\2_0\\2_0.xtc waitfor all
+animate write lammpstrj J:\\TrjData\\64me_290_\\2_0.lammpstrj skip 10+ sel [atomselect top "name OW HW1 HW2"]  waitfor all
+mol delete top
+
